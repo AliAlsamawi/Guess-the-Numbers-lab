1905def1a4f888992eab082572da845a58b61f65
Ali A <alsamawiali4@gmail.com>
2021-10-02 00:48:42 -0400

    start

--- app.js
+++ app.js
@@ -1,12 +1,38 @@
 console.log('Guess game party!')
 
+
 const game = {
-  title: 'Guess the Number!',
-  biggestNum: 100,
-  smallestNum: 1,
-  secretNum: null,
+
+title: 'Guess the Number!',
+
+biggestNum: 100,
+
+smallestNum: 1,
+
+secretNum: null,
+
+prevGuesses :[],
+
+getGuess(){
+
+  let currentGuess = prompt (`Enter a guss between ${this.
+
+    smallestNum} and ${this.biggestNum}`)
+
+    console.log(currentGuess)
+
+  },
+
   play: function() {
+
     this.secretNum = Math.floor(Math.random() * 
-      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+
+    (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+    while (this.smallestNum>= 1 && this.biggestNum <= 100){
+      i++;
+    
+    
+    }
   }
 }
+game.getGuess()
