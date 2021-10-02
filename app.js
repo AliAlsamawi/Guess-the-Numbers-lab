99d3addeb24ba8b6cca2437b16cf623cea647909
Ali A <alsamawiali4@gmail.com>
2021-10-02 16:26:56 -0400

    solve num 3

--- app.js
+++ app.js
@@ -1,38 +1,24 @@
-console.log('Guess game party!')
-
-
 const game = {
-
-title: 'Guess the Number!',
-
-biggestNum: 100,
-
-smallestNum: 1,
-
-secretNum: null,
-
-prevGuesses :[],
-
-getGuess(){
-
-  let currentGuess = prompt (`Enter a guss between ${this.
-
-    smallestNum} and ${this.biggestNum}`)
-
-    console.log(currentGuess)
-
-  },
-
+  title: 'Guess the Number!',
+  biggestNum: 100,
+  smallestNum: 1,
+  secretNum: null,
+  prevGuesses: [],
+  getGuess() {
+    let userInput = prompt (`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`); {
+      while (userInput > this.biggestNum || userInput < this.smallestNum) {
+        userInput = prompt (`That's wrong dude, choose a number between ${this.biggestNum} and ${this.smallestNum}. Please try again.`)
+      }
+      
+    }  
+},
   play: function() {
-
     this.secretNum = Math.floor(Math.random() * 
-
-    (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
-    while (this.smallestNum>= 1 && this.biggestNum <= 100){
-      i++;
-    
-    
-    }
-  }
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+      while (smallestNum >= 1 && biggestNum <= 100) {
+        ;
+      }
+    },
 }
+
 game.getGuess()
