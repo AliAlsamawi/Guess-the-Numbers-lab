76f12822ba079fef685dfe91155f05f08d59893e
Ali A <alsamawiali4@gmail.com>
2021-10-01 21:13:39 -0400

    slove

--- app.js
+++ app.js
@@ -1,2 +1,12 @@
-const game()
-console.log
+console.log('Guess game party!')
+
+const game = {
+  title: 'Guess the Number!',
+  biggestNum: 100,
+  smallestNum: 1,
+  secretNum: null,
+  play: function() {
+    this.secretNum = Math.floor(Math.random() * 
+      (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
+  }
+}
