473d490ce9aabbe501854ec0904b20a156d93d36
Ali A <alsamawiali4@gmail.com>
2021-10-03 22:35:28 -0400

    final 5

--- app.js
+++ app.js
@@ -1,25 +1,50 @@
 const game = {
   title: 'Guess the Number!',
-  biggestNum: 100,
+  biggestNum: 10,
   smallestNum: 1,
   secretNum: null,
   prevGuesses: [],
+  
+  
   play: function() {
     this.secretNum = Math.floor(Math.random() * 
       (this.biggestNum - this.smallestNum + 1)) + this.smallestNum
 
-      console.log(play)
+      do{
+        this.prevGuesses.push(this.getGuess())
+        this.render()
+      } while (this.prevGuesses[this.prevGuesses.length-1] != this.secretNum
+        );
+
     },
     
-
-    getGuess() {
+    getGuess(){
       let userInput = parseInt(prompt (`Enter a guess between ${this.smallestNum} and ${this.biggestNum}`))
-        while (userInput > this.biggestNum || userInput < this.smallestNum || isNaN(userInput)) {
-          userInput = parseInt(prompt (`This is wrong buddy, the guess must be between ${this.biggestNum} and ${this.smallestNum}. Trying again dude!`))
-        
-        } console.log(userInput)
-  }
 
-}
+        while (userInput > this.biggestNum || userInput < this.
+          smallestNum || isNaN(userInput)) {
+          
+            userInput = parseInt(prompt (`This is wrong buddy, 
+            
+            the guess must be between ${this.biggestNum} and $
+            
+            {this.smallestNum}. Trying again dude!`))
+          // console.log(getGuess)
+        } 
+        return userInput
+  
+  
+      },
 
-game.getGuess()
+
+      render() {
+        if (this.prevGuesses[this.prevGuesses.length -1] > this.secretNum) {
+          alert(`Your guess was too high, try again! Previous guesses were ${this.prevGuesses.join()}`)
+        } else if (this.prevGuesses[this.prevGuesses.length -1] < this.secretNum) {
+          alert(`Your guess was too low, try again! Previous guesses were ${this.prevGuesses.join()}`)
+        } else if ( this.prevGuesses[this.prevGuesses.length -1] === this.secretNum) {
+          alert(`Right on the money! Your guess was the secret number ${this.secretNum}! Previous guesses were ${this.prevGuesses.join()}.`)
+        }
+      }
+    }
+game.play()
